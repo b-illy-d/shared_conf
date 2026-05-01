bf3da4fcf0f87171b793d59c42698595d4021aa5
Billy Duraney <william.duraney@gmail.com>
2026-05-01 12:01:38 -0400

    update keystrokes

--- nvim/lua/plugins/pi.lua
+++ nvim/lua/plugins/pi.lua
@@ -3,7 +3,7 @@ return {
     "carderne/pi-nvim",
     config = function()
       require("pi-nvim").setup()
-      vim.keymap.set("n", "<leader>pp", ":PiSend<CR>")
+      vim.keymap.set("n", "<leader>pp", ":PiSendFile<CR>")
       vim.keymap.set("v", "<leader>pp", ":PiSendSelection<CR>")
       vim.keymap.set({ "n", "v" }, "<leader>pf", ":PiSendFile<CR>")
       vim.keymap.set({ "n", "v" }, "<leader>pb", ":PiSendBuffer<CR>")
